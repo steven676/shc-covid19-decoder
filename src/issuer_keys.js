63eeb440ecc119a6aade8a035fd97771644654eb
Steven Luo <steven@steven676.net>
2021-08-10 11:06:25 -0700

    Add signing key for New York State Excelsior Pass Plus
    Key from https://ekeys.ny.gov/epass/doh/dvc/2021/.well-known/jwks.json:
    
    $ sha256sum jwks.json
    b1e12070123acae094fd0047ae4d6b2d8aabe2f1c592932a224e83b83c9d6e2e  jwks.json

--- src/issuer_keys.js
+++ src/issuer_keys.js
@@ -15,6 +15,14 @@ const issuerKeys = [
     x: "3dQz5ZlbazChP3U7bdqShfF0fvSXLXD9WMa1kqqH6i4",
     y: "FV4AsWjc7ZmfhSiHsw2gjnDMKNLwNqi2jMLmJpiKWtE",
   },
+  {
+    id: "us.ny-001",
+    iss: "https://ekeys.ny.gov/epass/doh/dvc/2021",
+    kid: "9ENs36Gsu-GmkWIyIH9XCozU9BFhLeaXvwrT3B97Wok",
+    alg: "ES256", kty: "EC", crv: "P-256", use: "sig",
+    x: "--M0AedrNg31sHZGAs6qg7WU9LwnDCMWmd6KjiKfrZU",
+    y: "rSf2dKerJFW3-oUNcvyrI2x39hV2EbazORZvh44ukjg",
+  },
 ];
 
 module.exports = {
