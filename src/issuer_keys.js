8bcf3b8b3e3420606b9690992b73c14766641c1b
Steven Luo <steven@steven676.net>
2021-08-18 04:17:17 -0700

    Add signing key for state of Louisiana LA Wallet
    Key from https://healthcardcert.lawallet.com/.well-known/jwks.json:
    
    $ sha256sum jwks.json
    fede561fb56d82e82df8a6e5f5f2679460b7a4051a927ec318decf686199b633  jwks.json

--- src/issuer_keys.js
+++ src/issuer_keys.js
@@ -23,6 +23,14 @@ const issuerKeys = [
     x: "--M0AedrNg31sHZGAs6qg7WU9LwnDCMWmd6KjiKfrZU",
     y: "rSf2dKerJFW3-oUNcvyrI2x39hV2EbazORZvh44ukjg",
   },
+  {
+    id: "us.la-001",
+    iss: "https://healthcardcert.lawallet.com",
+    kid: "UOvXbgzZj4zL-lt1uJVS_98NHQrQz48FTdqQyNEdaNE",
+    alg: "ES256", kty: "EC", crv: "P-256", use: "sig",
+    x: "n1PxhSk7DQj8ZBK3VIfwhlcN__QG357gbiTfZYt1gn8",
+    y: "ZDGv5JYe4mCm75HCsHG8UkIyffr1wcZMwJjH8v5cGCc",
+  },
 ];
 
 module.exports = {
